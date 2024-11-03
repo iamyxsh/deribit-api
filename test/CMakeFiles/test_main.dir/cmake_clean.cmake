2099898d319b06e08ac7c29beafd9440855cd8de
Yash Sharma <iamyxsh@gmail.com>
2024-11-03 12:19:10 +0530

    project completed

---
+++ test/CMakeFiles/test_main.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_main.dir/test_derbit_api.o"
+  "CMakeFiles/test_main.dir/test_derbit_api.o.d"
+  "CMakeFiles/test_main.dir/test_main.o"
+  "CMakeFiles/test_main.dir/test_main.o.d"
+  "test_main"
+  "test_main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ test/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /Users/iamyxsh/Personal/test-cpp/test
+# Build directory: /Users/iamyxsh/Personal/test-cpp/build/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(DerbitAPITest "/Users/iamyxsh/Personal/test-cpp/build/test/test_main")
+set_tests_properties(DerbitAPITest PROPERTIES  _BACKTRACE_TRIPLES "/Users/iamyxsh/Personal/test-cpp/test/CMakeLists.txt;18;add_test;/Users/iamyxsh/Personal/test-cpp/test/CMakeLists.txt;0;")
+subdirs("googletest")
